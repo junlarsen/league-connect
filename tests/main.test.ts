a8d269e480397de77599b869d440d633f7a5f9b7
supergrecko <me@supergrecko.com>
2020-01-31 20:55:10 +0100

    tests: add more tests with different slash prefixes

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,47 +2,115 @@ import { Response } from 'node-fetch'
 import * as Connector from '../src'
 
 describe('grabbing and using credentials', () => {
-  test('tests grabbing credentials', async (done) => {
-    const conn = await Connector.auth()
+  describe('finding credentials', () => {
+    test('tests grabbing credentials', async (done) => {
+      const conn = await Connector.auth()
 
-    expect(conn).not.toBeUndefined()
+      expect(conn).not.toBeUndefined()
 
-    done()
+      done()
+    })
   })
 
-  test('websocket also works when passing credentials', async (done) => {
-    const ws = await Connector.connect()
-    expect(ws).not.toBeUndefined()
+  describe('sending requests', () => {
+    test('basic request does not result in a http error', async (done) => {
+      const res = await Connector.request({
+        method: 'POST',
+        url: 'Help'
+      })
 
-    ws.on('open', () => {
-      ws.close()
+      expect(res).toBeInstanceOf(Response)
+      expect(res.ok).toEqual(true)
+
+      done()
     })
 
-    done()
+    test('request with slash prefix also works', async (done) => {
+      const res = await Connector.request({
+        method: 'POST',
+        url: '/Help'
+      })
+
+      expect(res).toBeInstanceOf(Response)
+      expect(res.ok).toEqual(true)
+
+      done()
+    })
   })
 
-  test('websocket connects correctly without credentials', async (done) => {
-    const conn = await Connector.auth()
-    const ws = await Connector.connect(conn)
+  describe('using the websocket', () => {
+    test('websocket also works when passing credentials', async (done) => {
+      const ws = await Connector.connect()
+      expect(ws).not.toBeUndefined()
 
-    expect(ws).toBeInstanceOf(Connector.LeagueWebSocket)
+      ws.on('open', () => {
+        ws.close()
+      })
 
-    ws.on('open', () => {
-      ws.close()
+      done()
     })
 
-    done()
+    test('websocket connects correctly without credentials', async (done) => {
+      const conn = await Connector.auth()
+      const ws = await Connector.connect(conn)
+
+      expect(ws).toBeInstanceOf(Connector.LeagueWebSocket)
+
+      ws.on('open', () => {
+        ws.close()
+      })
+
+      done()
+    })
   })
 
-  test('basic request does not result in a http error', async (done) => {
-    const res = await Connector.request({
-      method: 'POST',
-      url: 'Help'
+  describe('subscribing to the websocket', () => {
+    test('subscription with slashes url works', async (done) => {
+      const conn = await Connector.auth()
+      const ws = await Connector.connect(conn)
+
+      ws.on('open', () => {
+        ws.subscribe('/lol-chat/v1/conversations/active', () => 0)
+
+        expect(ws.getListeners()['/lol-chat/v1/conversations/active']).toHaveLength(1)
+
+        ws.close()
+      })
+
+      done()
     })
 
-    expect(res).toBeInstanceOf(Response)
-    expect(res.ok).toEqual(true)
+    test('subscribing multiple times works as well', async (done) => {
+      const conn = await Connector.auth()
+      const ws = await Connector.connect(conn)
 
-    done()
+      ws.on('open', () => {
+        ws.subscribe('/lol-chat/v1/conversations/active', () => 0)
+        ws.subscribe('/lol-chat/v1/conversations/active', () => 0)
+        ws.subscribe('/lol-chat/v1/conversations/active', () => 0)
+        ws.subscribe('/lol-chat/v1/conversations/active', () => 0)
+
+        expect(ws.getListeners()['/lol-chat/v1/conversations/active']).toHaveLength(4)
+
+        ws.close()
+      })
+
+      done()
+    })
+
+    test('subscribing with non-slash prefixed path works', async (done) => {
+      const conn = await Connector.auth()
+      const ws = await Connector.connect(conn)
+
+      ws.on('open', () => {
+        ws.subscribe('lol-chat/v1/conversations/active', () => 0)
+
+        expect(ws.getListeners()['/lol-chat/v1/conversations/active']).toHaveLength(1)
+
+        ws.close()
+      })
+
+      done()
+    })
   })
 })
