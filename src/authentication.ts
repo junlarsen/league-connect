7fb6b7303a60b78397f3671566695794b12a450a
infister <mateuszdul@gmail.com>
2022-12-03 22:19:22 +0100

    Update authentication.ts

--- src/authentication.ts
+++ src/authentication.ts
@@ -115,7 +115,7 @@ export async function authenticate(options?: AuthenticationOptions): Promise<Cre
   async function tryAuthenticate() {
     const name = options?.name ?? DEFAULT_NAME
     const portRegex = /--app-port=([0-9]+)/
-    const passwordRegex = /--remoting-auth-token=([\w-_]+)/
+    const passwordRegex = /--remoting-auth-token=([a-zA-Z0-9_-]+)([\s]*--[a-zA-Z]*)/
     const pidRegex = /--app-pid=([0-9]+)/
     const isWindows = process.platform === 'win32'
 
